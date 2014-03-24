aa470b70ed7a52c07ebd1479168085115aa3ddac
Geir Ove Grønmo <grove@geirove.org>
2014-03-24 11:32:12 +0100

    Virtuoso now works.

--- virtuoso/load_data.sh
+++ virtuoso/load_data.sh
@@ -1,4 +1,5 @@
 DATA_ROOT=$HOME/bsbmtools-0.2
-time curl -D- -H 'Content-Type: text/turtle' --upload-file $DATA_ROOT/dataset_5m.ttl -X POST 'http://localhost:8890/sparql-graph-crud?graph-uri=http://example.org/test'
 
-# isql: grant SPARQL_UPDATE to "SPARQL";
+/usr/local/virtuoso-opensource/bin/isql localhost dba dba /vagrant/dbsetup.isql
+
+time curl -H 'Content-Type: text/turtle' -T $DATA_ROOT/dataset_5m.ttl -X POST 'http://localhost:8890/sparql-graph-crud?graph-uri=http://example.org/test'
